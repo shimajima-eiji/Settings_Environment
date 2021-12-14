0de39bf0a89eb5a5bbf09d696509d44ba164b0ed
のむら屋ごろう <shimajima-eiji@live.jp>
2021-12-14 23:34:18 +0900

    fix: 一年芝スクリプトを毎日コントリの振る舞いに寄せた

--- for_github/contribution365.sh
+++ for_github/contribution365.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 update_file=dummy
 
+if [ -z "$1" ]
+then
+  echo "[stop] Require argument: path(git clone directory)"
+  exit 1
+else
+  git_clone_path=$1
+fi
+
+cd ${git_clone_path}
+
 for now_day in $(seq 0 366)  # 当日を含めるので0からカウント
 do
   message="dummy update: $(date -d "-${now_day} days" "+%Y/%m/%d")"
