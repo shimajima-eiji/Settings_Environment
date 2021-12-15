2179fcb4e68c32d47b552bcfc8a790e09e07c112
中　翔 <shimajima-eiji@live.jp>
2021-12-15 10:01:56 +0900

    Create choco_install.bat

---
+++ chocolatey/choco_install.bat
@@ -0,0 +1,6 @@
+@echo off
+set config_path=(chocolatey.config)
+
+rem "need Administrator"
+@powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
+choco install %config_path%
