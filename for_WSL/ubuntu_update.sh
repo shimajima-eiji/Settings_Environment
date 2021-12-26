d3eb8c003f5bc16d9a177e4337fcc7d2f86fbf3c
中　翔 <shimajima-eiji@live.jp>
2021-12-26 14:37:42 +0900

    Update ubuntu_update.sh

--- for_WSL/ubuntu_update.sh
+++ for_WSL/ubuntu_update.sh
@@ -1,9 +1,33 @@
 #!/bin/sh
-source ~/.env
-# `.env`にはSUDO_PASSを設定している
-echo ${SUDO_PASS} | sudo -S su
+# curlでの実行に対応している
 
-# suに失敗した時に動かないようにする意味も含めて、明示的にsudoを書かない（書く必要がない）
+# 基本的には.envに書き込まれている
+if [ -f "~/.env" ]
+then
+  source ~/.env
+fi
+
+# 標準入力があれば優先する（セキュリティの観点から非推奨）
+SUDO_PASS=${1:-${SUDO_PASS}}
+
+# sudoに失敗した場合は実行しない
+if [ -z "${SUDO_PASS}" ]
+then
+  echo "[Stop]Need pass of sudo."
+  echo "[Hint] create ~/.env. after 'SUDO_PASS=(your password)'."
+  exit 1
+fi
+
+# ルートユーザーにスイッチできなかった場合は実行しない
+echo "${1:-${SUDO_PASS}}" | sudo -S su
+if [ ! "${USER}" = "root" ]
+then
+  echo "[Stop]Failed 'su'."
+  echo "[Hint]Wrong sudo pass? view SUDO_PASS of '~/.env'"
+  exit 1
+fi
+
+# 以下、rootユーザーで実施
 apt update
 apt upgrade -y
 apt full-upgrade -y
