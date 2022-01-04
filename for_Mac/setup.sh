12b5d069208d101870309430b43ab0490011abb1
中　翔 <shimajima-eiji@live.jp>
2022-01-04 13:30:12 +0900

    Update setup.sh

--- for_Mac/setup.sh
+++ for_Mac/setup.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
 
-# brew
+### brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+### node
+brew install nodenv
+# nodenv install (最新版)
+node install -g github-changes
+
+brew install rbenv
+# rbenv install (最新版)
+gem install -g github_changelog_generator
+
+### python
+brew install pyenv
+### pyenv install (最新版)
+pip install pandas
