7c679a0786ff26a0119a85f302597ecaadbfd2bf
中　翔 <shimajima-eiji@live.jp>
2022-01-09 16:39:29 +0900

    Create 最低限のSSH設定.sh

---
+++ for_SSH/最低限のSSH設定.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+: <<COMMENT
+説明のため、以下と仮定する。
+- サーバーユーザー: ssh_server
+- ユーザーパスワード: 有り
+- サーバーのIPアドレス: 192.168.1.1
+  - IPアドレスは`hostname -I`(アイ)で確認できる
+COMMENT
+
+# サーバー側: ssh_serverユーザーで実施
+sudo ssh-keygen -A
+sed -i -e "s/^PasswordAuthentication no/PasswordAuthentication yes/" /etc/ssh/sshd_config  # vi /etc/ssh/sshd_configで変更するのと同じ
+sudo service ssh restart
+
+# クライアント側: 何でもOK
+ssh ssh_server@192.168.1.1
